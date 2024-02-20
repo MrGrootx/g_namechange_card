afbb0860c7ab60e154505d843664c090fd5569ac
Mr Groot <113226362+MrGrootx@users.noreply.github.com>
2024-02-20 16:47:18 +0530

    feat: readme % codes

--- shared/Config.lua
+++ shared/Config.lua
@@ -3,5 +3,5 @@ Config = {}
 
 Config.Discord = {
    webhookURL =
-   "https://discord.com/api/webhooks/1191336225940975666/1goKGee7YHVKcqdYzFnAG2QdBTnmwVHDdER6XmCCmtzdf8-0Np2cTdHPmtTpbiTQVXfY"
+   ""
 }
